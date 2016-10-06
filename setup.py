cee18639da89bc4d293b19481fa8d1147e5edefc
ANovokmet <ante.novokmet@fer.hr>
2016-10-06 23:11:32 +0200

    Tarballz

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
   author = 'Ante Novokmet',
   author_email = 'ante.je.zakon@gmail.com',
   url = 'https://github.com/ANovokmet/fer2py/', 
+  download_url = 'https://github.com/ANovokmet/fer2py/tarball/0.1',
   install_requires = ['beautifulsoup4', 'requests']
 )
